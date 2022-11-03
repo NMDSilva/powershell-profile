3ff2c45049b7f0b981b2e600227c0e11c98b0128
Nuno Silva <Nuno.D.Silva@seg-social.pt>
2022-11-03 14:26:07 -0100

    feat: :sparkles: novas funcoes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -21,25 +21,24 @@ $isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administ
 # If so and the current host is a command line, then change to red color 
 # as warning to user that they are operating in an elevated context
 # Useful shortcuts for traversing directories
-function cd...  { Set-Location ..\.. }
+function cd... { Set-Location ..\.. }
 function cd.... { Set-Location ..\..\.. }
 
 # Compute file hashes - useful for checking successful downloads 
-function md5    { Get-FileHash -Algorithm MD5 $args }
-function sha1   { Get-FileHash -Algorithm SHA1 $args }
+function md5 { Get-FileHash -Algorithm MD5 $args }
+function sha1 { Get-FileHash -Algorithm SHA1 $args }
 function sha256 { Get-FileHash -Algorithm SHA256 $args }
 
 # Quick shortcut to start notepad
-function n      { notepad $args }
+function n { notepad $args }
 
 # Drive shortcuts
-function HKLM:  { Set-Location HKLM: }
-function HKCU:  { Set-Location HKCU: }
-function Env:   { Set-Location Env: }
+function HKLM: { Set-Location HKLM: }
+function HKCU: { Set-Location HKCU: }
+function Env: { Set-Location Env: }
 
 # Creates drive shortcut for Work Folders, if current user account is using it
-if (Test-Path "$env:USERPROFILE\Work Folders")
-{
+if (Test-Path "$env:USERPROFILE\Work Folders") {
     New-PSDrive -Name Work -PSProvider FileSystem -Root "$env:USERPROFILE\Work Folders" -Description "Work Folders"
     function Work: { Set-Location Work: }
 }
@@ -47,33 +46,26 @@ if (Test-Path "$env:USERPROFILE\Work Folders")
 # Set up command prompt and window title. Use UNIX-style convention for identifying 
 # whether user is elevated (root) or not. Window title shows current version of PowerShell
 # and appends [ADMIN] if appropriate for easy taskbar identification
-function prompt 
-{ 
-    if ($isAdmin) 
-    {
+function prompt { 
+    if ($isAdmin) {
         "[" + (Get-Location) + "] # " 
     }
-    else 
-    {
+    else {
         "[" + (Get-Location) + "] $ "
     }
 }
 
 $Host.UI.RawUI.WindowTitle = "PowerShell {0}" -f $PSVersionTable.PSVersion.ToString()
-if ($isAdmin)
-{
+if ($isAdmin) {
     $Host.UI.RawUI.WindowTitle += " [ADMIN]"
 }
 
 # Does the the rough equivalent of dir /s /b. For example, dirs *.png is dir /s /b *.png
-function dirs
-{
-    if ($args.Count -gt 0)
-    {
+function dirs {
+    if ($args.Count -gt 0) {
         Get-ChildItem -Recurse -Include "$args" | Foreach-Object FullName
     }
-    else
-    {
+    else {
         Get-ChildItem -Recurse | Foreach-Object FullName
     }
 }
@@ -81,16 +73,13 @@ function dirs
 # Simple function to start a new elevated process. If arguments are supplied then 
 # a single command is started with admin rights; if not then a new admin instance
 # of PowerShell is started.
-function admin
-{
-    if ($args.Count -gt 0)
-    {   
-       $argList = "& '" + $args + "'"
-       Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
+function admin {
+    if ($args.Count -gt 0) {   
+        $argList = "& '" + $args + "'"
+        Start-Process "powershell.exe" -Verb runAs -ArgumentList $argList
     }
-    else
-    {
-       Start-Process "$psHome\powershell.exe" -Verb runAs
+    else {
+        Start-Process "powershell.exe" -Verb runAs
     }
 }
 
@@ -101,14 +90,11 @@ Set-Alias -Name sudo -Value admin
 
 
 # Make it easy to edit this profile once it's installed
-function Edit-Profile
-{
-    if ($host.Name -match "ise")
-    {
+function Edit-Profile {
+    if ($host.Name -match "ise") {
         $psISE.CurrentPowerShellTab.Files.Add($profile.CurrentUserAllHosts)
     }
-    else
-    {
+    else {
         code $profile.CurrentUserAllHosts
     }
 }
@@ -118,30 +104,29 @@ function Edit-Profile
 Remove-Variable identity
 Remove-Variable principal
 
-Function Test-CommandExists
-{
+Function Test-CommandExists {
     Param ($command)
     $oldPreference = $ErrorActionPreference
     $ErrorActionPreference = 'SilentlyContinue'
-    try {if(Get-Command $command){RETURN $true}}
-    Catch {Write-Host "$command does not exist"; RETURN $false}
-    Finally {$ErrorActionPreference=$oldPreference}
+    try { if (Get-Command $command) { RETURN $true } }
+    Catch { Write-Host "$command does not exist"; RETURN $false }
+    Finally { $ErrorActionPreference = $oldPreference }
 } 
 
 function ll { Get-ChildItem -Path $pwd -File }
 function g { Set-Location d:\apps\ }
-function gcom
-{
-	git add .
-	git commit -m "$args"
+function gcom {
+    git add .
+    git commit -m "$args"
 }
 
 Function Get-PubIP {
     (Invoke-WebRequest http://ifconfig.me/ip ).Content
 }
 function uptime {
-    Get-WmiObject win32_operatingsystem | Select-Object csname, @{LABEL='LastBootUpTime';
-    EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
+    Get-WmiObject win32_operatingsystem | Select-Object csname, @{LABEL = 'LastBootUpTime';
+        EXPRESSION                                                      = { $_.ConverttoDateTime($_.lastbootuptime) }
+    }
 }
 function reload-profile {
     & $profile
@@ -154,36 +139,81 @@ function find-file($name) {
 }
 function unzip ($file) {
     Write-Output("Extracting", $file, "to", $pwd)
-	$fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object{$_.FullName}
+    $fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object { $_.FullName }
     Expand-Archive -Path $fullFile -DestinationPath $pwd
 }
 function grep($regex, $dir) {
     if ( $dir ) {
-        Get-ChildItem $dir | select-string $regex
+        Get-ChildItem $dir | Select-String $regex
         return
     }
-    $input | select-string $regex
+    $input | Select-String $regex
 }
 function touch($file) {
     "" | Out-File $file -Encoding ASCII
 }
 function df {
-    get-volume
+    Get-Volume
 }
-function sed($file, $find, $replace){
+function sed($file, $find, $replace) {
     (Get-Content $file).replace("$find", $replace) | Set-Content $file
 }
 function which($name) {
     Get-Command $name | Select-Object -ExpandProperty Definition
 }
 function export($name, $value) {
-    set-item -force -path "env:$name" -value $value;
+    Set-Item -force -path "env:$name" -value $value;
 }
 function pkill($name) {
     Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
 }
 function pgrep($name) {
-    Get-Process $name
+    if ((Get-Process $name -ErrorAction SilentlyContinue).length -eq 0) {
+        Write-Output("Não existem processos para o filtro aplicado")
+    }
+    else {
+        Get-Process $name
+    }
+}
+
+function ipauto() {
+    Write-Output("IP Auto")
+    $ip = Get-NetIPConfiguration -InterfaceAlias "Ethernet"
+    $net = $ip.NetIPv4Interface
+    Remove-NetRoute -InterfaceIndex $net.InterfaceIndex -Confirm:$false
+    Set-NetIPInterface -InterfaceIndex $net.InterfaceIndex -Dhcp Enabled -AdvertiseDefaultRoute Enabled
+    Set-DnsClientServerAddress -InterfaceIndex $net.InterfaceIndex -ResetServerAddresses
+    ipconfig /renew
+}
+
+function ipfixo() {
+    $ip = Get-NetIPConfiguration -InterfaceAlias "Ethernet"
+    $net = $ip.NetIPv4Interface
+    Set-NetIPInterface -InterfaceIndex $net.InterfaceIndex -Dhcp Disabled
+    New-NetIPAddress -InterfaceIndex $net.InterfaceIndex -IPAddress $ip.IPv4Address.IPAddress -PrefixLength 24 -DefaultGateway 172.28.128.30
+    Set-DnsClientServerAddress -InterfaceIndex $net.InterfaceIndex -ServerAddresses 8.8.8.8
+}
+
+function restartpool {
+    param(
+        [Parameter(Mandatory = $true)]
+        [Alias('server')]
+        [AllowNull()]
+        ${server[SERV128012]},
+        [Parameter(Mandatory = $true)]
+        [string]$pool
+    )
+    $user = "acores\ns198422adm"
+
+    $server = 
+    if (${server[SERV128012]}) {
+        ${server[SERV128012]}
+    }
+    else {
+        'SERV128012'
+    }
+
+    Invoke-Command -ComputerName $server -Credential $user -ScriptBlock { param($p) Restart-WebAppPool -Name $p } -ArgumentList $pool
 }
 
 
@@ -197,5 +227,5 @@ oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expre
 # See https://ch0.co/tab-completion for details.
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+    Import-Module "$ChocolateyProfile"
 }
